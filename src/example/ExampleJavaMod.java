84397b1addac94cec9ee8c731bb3ba31db3225dc
ImCocos <90627867+ImCocos@users.noreply.github.com>
2021-10-27 17:35:53 +0200

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,36 +1,14 @@
-package example;
-
-import arc.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
-import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
-
-public class ExampleJavaMod extends Mod{
-
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    String name = "Adam";
+    System.out.print("Hello " + name +"\r\n");
+    if (name == "Adam") 
+    {
+      System.out.print("Special user priveledges granted!");
     }
-
-    @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
-    }
-
+  }
+  
+}
 }
